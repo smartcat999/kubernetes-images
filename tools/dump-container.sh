838e544623e742eb36f0bac90a788b1702350338
peng wu <2030047311@qq.com>
2023-04-10 14:31:42 +0800

    feat: update container check script

---
+++ tools/dump-container.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+target=${target:-192.168.200.3 192.168.200.4 192.168.200.5 192.168.200.6 192.168.200.7}
+# shellcheck disable=SC2068
+for elem in ${target[@]}; do
+  scp image-check.sh "root@$elem:/root/"
+done
+
+function dump_root_containers {
+  # shellcheck disable=SC2068
+  for elem in ${target[@]}; do
+    ssh "root@$elem" "/root/image-check.sh root"
+  done
+}
+
+function dump_tools_containers {
+  # shellcheck disable=SC2068
+  for elem in ${target[@]}; do
+    ssh "root@$elem" "/root/image-check.sh tools"
+  done
+}
+
+function dump_envs_containers {
+  # shellcheck disable=SC2068
+  for elem in ${target[@]}; do
+    ssh "root@$elem" "/root/image-check.sh env"
+  done
+}
+
+CMD=$1
+
+if [ "$CMD" = "root" ]; then
+  dump_root_containers
+elif [ "$CMD" = "tools" ]; then
+  dump_tools_containers
+elif [ "$CMD" = "env" ]; then
+  dump_envs_containers
+fi
---
+++ tools/image-check.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#set -x
+function utils {
+  if [ ${debug:-false} = true ]; then
+    set -x
+  fi
+
+  CMD=$1
+  if [ "$CMD" = "privileged" ]; then
+    docker ps --quiet -a | xargs docker inspect --format='{{.Name}} {{.HostConfig.Privileged}}' | grep true | awk '{print $1}'
+
+  elif [ "$CMD" = "root" ]; then
+    containers=$(docker ps | awk 'NR!=1 {print $1}')
+    # shellcheck disable=SC2154
+    #  echo $containers
+    # shellcheck disable=SC2068
+    for container in ${containers[@]}; do
+      # echo $image
+      # shellcheck disable=SC2046
+      # shellcheck disable=SC1066
+      user=$(docker exec -i "$container" whoami)
+      if [ $? != 0 ]; then
+        continue
+      fi
+      if [ "$user" = "root" ]; then
+        container_info=$(docker ps --format="{{.ID}}  {{.Image}}  {{.Names}}" | grep "$container")
+        imageid=$(docker ps | grep "$container" | awk '{print $2}')
+        image_info=$(docker inspect $imageid --format="{{index .RepoTags 0}}")
+        echo "$container_info $image_info"
+      fi
+    done
+  elif [ "$CMD" = "tools" ]; then
+    # tools=("tcpdump" "sniffer" "wireshark" "Netcat" "gdb" "strace" "readelf" "cpp" "gcc" "dexdump" "mirror" "JDK" "netcat")
+    tools=("tcpdump" "sniffer" "wireshark" "Netcat" "strace" "readelf" "Nmap" "gdb" "cpp" "gcc" "jdk" "javac" "make" "binutils" "flex" "glibc-devel" "gcc-c++" "Id" "lex" "rpcgen" "objdump" "eu-readelf" "eu-objdump" "dexdump" "mirror" "lua" "Perl")
+    # shellcheck disable=SC2068
+    for tool in ${tools[@]}; do
+      echo "$tool"
+      # shellcheck disable=SC2046
+      # shellcheck disable=SC1066
+      overlays=$(find /var/lib/docker | grep -i "/${tool}$" | awk -F/ '{print $6}' | uniq | sort | grep -v "^$")
+      if [ "$overlays" = "" ]; then
+        continue
+      fi
+      docker image ls | awk '{if (NR>1){print $3}}' | xargs docker inspect --format '{{.Id}}, {{.RepoTags}}, {{.GraphDriver.Data}}' | grep -E $(echo $overlays | sed 's/ /|/g') | awk -F, '{print $1 $2}'
+      # shellcheck disable=SC2181
+      if [ $? != 0 ]; then
+        continue
+      fi
+    done
+  elif [ "$CMD" = "env" ]; then
+    containers=$(docker ps | awk 'NR!=1 {print $1}')
+    # shellcheck disable=SC2068
+    for container in ${containers[@]}; do
+      # echo $image
+      # shellcheck disable=SC2046
+      # shellcheck disable=SC1066
+      container_info=$(docker ps --format="{{.ID}}  {{.Image}}  {{.Names}}" | grep "$container")
+      envs=$(docker inspect "$container" --format="{{.Config.Env}}")
+      # shellcheck disable=SC2046
+      if [ "$(echo "$envs" | grep -i "password\|secret\|token")" = "" ]; then
+        continue
+      fi
+      imageid=$(docker ps | grep "$container" | awk '{print $2}')
+      image_info=$(docker inspect "$imageid" --format="{{index .RepoTags 0}}")
+      # shellcheck disable=SC2181
+      if [ $? = 0 ]; then
+        echo "$container_info $image_info $envs"
+      else
+        echo "$container_info $imageid $envs"
+      fi
+    done
+  fi
+}
+
+utils $1
