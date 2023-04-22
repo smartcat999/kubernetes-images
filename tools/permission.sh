df121a21a030eb3edde0bfd282cdff359b70f24a
peng wu <2030047311@qq.com>
2023-04-22 11:22:27 +0800

    feat: update conf permission

--- tools/permission.sh
+++ tools/permission.sh
@@ -3,11 +3,17 @@
 
 # shellcheck disable=SC2113
 function change_permission {
-  etc=$(find $1 ! -perm 600 -name "*.crt" -o ! -perm 600 -name "*.pem" -o ! -perm 600 -name "*.conf" 2>/dev/null)
+  crt=$(find $1 ! -perm 600 -name "*.crt" -o ! -perm 600 -name "*.pem" 2>/dev/null)
   # shellcheck disable=SC2068
-  for elem in ${etc[@]}; do
+  for elem in ${crt[@]}; do
     chmod 600 $elem
   done;
+
+  conf=$(find $1 ! -perm 640 -name "*.conf" 2>/dev/null)
+  # shellcheck disable=SC2068
+    for elem in ${conf[@]}; do
+      chmod 640 $elem
+    done;
 }
 
 change_permission /etc
