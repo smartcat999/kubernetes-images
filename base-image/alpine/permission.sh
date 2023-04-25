4fe3d07e38190e2bc2d94c7556c1952545eebca4
peng wu <2030047311@qq.com>
2023-04-25 16:19:23 +0800

    feat: update base-image dockerfile

---
+++ base-image/alpine/permission.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+# shellcheck disable=SC2113
+function change_permission {
+  crt=$(find $1 ! -perm 600 -name "*.crt" -o ! -perm 600 -name "*.pem" 2>/dev/null)
+  # shellcheck disable=SC2068
+  for elem in ${crt[@]}; do
+    chmod 600 $elem
+  done;
+
+  conf=$(find $1 ! -perm 640 -name "*.conf" 2>/dev/null)
+  # shellcheck disable=SC2068
+    for elem in ${conf[@]}; do
+      chmod 640 $elem
+    done;
+}
+
+change_permission /etc
+change_permission /usr
