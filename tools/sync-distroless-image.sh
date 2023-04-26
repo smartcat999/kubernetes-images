8281d7c7691edec985c58a0cfcaa9cf7bd173006
peng wu <2030047311@qq.com>
2023-04-26 16:16:04 +0800

    feat: add build distroless image base-debian12 support

--- tools/sync-distroless-image.sh
+++ tools/sync-distroless-image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ ${debug:-flase} = "true" ]]; then
+  set -x
+fi
+
+CMD=$1
+
 REPO=${REPO:-2030047311}
 DEBIAN_VERSION=debian12
 ARCH=(
@@ -14,33 +20,66 @@ IMAGE_TAG=(
   nonroot-debug
 )
 
-# shellcheck disable=SC2068
-for tag in ${IMAGE_TAG[@]}; do
+function build-nodejs() {
   # shellcheck disable=SC2068
-  for version in ${NODEJS_VERSIONS[@]}; do
+  for tag in ${IMAGE_TAG[@]}; do
+    # shellcheck disable=SC2068
+    for version in ${NODEJS_VERSIONS[@]}; do
+      if [[ $tag = "nonroot" ]]; then
+        bazel build nodejs:${version}_nonroot_${DEBIAN_VERSION}
+      else
+        bazel build nodejs:${version}_debug_nonroot_${DEBIAN_VERSION}
+      fi
+    done
+  done
+
+  # shellcheck disable=SC2068
+  for tag in ${IMAGE_TAG[@]}; do
+    # shellcheck disable=SC2068
+    # shellcheck disable=SC2034
+    for version in ${NODEJS_VERSIONS[@]}; do
+      manifest=${REPO}/${version}-${DEBIAN_VERSION}:${tag}
+      for arch in ${ARCH[@]}; do
+        # shellcheck disable=SC2128
+
+        image=${REPO}/${version}-${DEBIAN_VERSION}:${tag}-${arch}
+        if [[ $tag = "nonroot" ]]; then
+          oci_dir=oci:bazel-bin/nodejs/${version}_nonroot_${arch}_${DEBIAN_VERSION}
+        else
+          oci_dir=oci:bazel-bin/nodejs/${version}_debug_nonroot_${arch}_${DEBIAN_VERSION}
+        fi
+        skopeo copy $oci_dir docker-daemon:$image
+        docker push $image
+        docker manifest create $manifest -a $image
+      done
+      docker manifest push $manifest
+    done
+  done
+}
+
+function build-base() {
+  # shellcheck disable=SC2068
+  for tag in ${IMAGE_TAG[@]}; do
     if [[ $tag = "nonroot" ]]; then
-      bazel build nodejs:${version}_nonroot_${DEBIAN_VERSION}
+      bazel build base:base_nonroot_${DEBIAN_VERSION}
     else
-      bazel build nodejs:${version}_debug_nonroot_${DEBIAN_VERSION}
+      bazel build base:debug_nonroot_${DEBIAN_VERSION}
     fi
   done
-done
 
-# shellcheck disable=SC2068
-for tag in ${IMAGE_TAG[@]}; do
   # shellcheck disable=SC2068
-  # shellcheck disable=SC2034
-
-  for version in ${NODEJS_VERSIONS[@]}; do
-    manifest=${REPO}/${version}-${DEBIAN_VERSION}:${tag}
+  for tag in ${IMAGE_TAG[@]}; do
+    # shellcheck disable=SC2068
+    # shellcheck disable=SC2034
+    manifest=${REPO}/base-${DEBIAN_VERSION}:${tag}
     for arch in ${ARCH[@]}; do
       # shellcheck disable=SC2128
 
-      image=${REPO}/${version}-${DEBIAN_VERSION}:${tag}-${arch}
+      image=${REPO}/base-${DEBIAN_VERSION}:${tag}-${arch}
       if [[ $tag = "nonroot" ]]; then
-        oci_dir=oci:bazel-bin/nodejs/${version}_nonroot_${arch}_${DEBIAN_VERSION}
+        oci_dir=oci:bazel-bin/base/base_nonroot_${arch}_${DEBIAN_VERSION}
       else
-        oci_dir=oci:bazel-bin/nodejs/${version}_debug_nonroot_${arch}_${DEBIAN_VERSION}
+        oci_dir=oci:bazel-bin/base/debug_nonroot_${arch}_${DEBIAN_VERSION}
       fi
       skopeo copy $oci_dir docker-daemon:$image
       docker push $image
@@ -48,4 +87,10 @@ for tag in ${IMAGE_TAG[@]}; do
     done
     docker manifest push $manifest
   done
-done
+}
+
+if [[ $CMD = "nodejs" ]]; then
+  build-nodejs
+elif [[ $CMD = "base" ]]; then
+  build-base
+fi
