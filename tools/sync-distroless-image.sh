9632d50dd006b8255f7cae50cdabc7a877df8c9e
peng wu <2030047311@qq.com>
2023-06-01 16:31:13 +0800

    feat: update distroless debian12 nossl support

--- tools/sync-distroless-image.sh
+++ tools/sync-distroless-image.sh
@@ -87,6 +87,37 @@ function build-base() {
   done
 }
 
+function build-base-nossl() {
+  # shellcheck disable=SC2068
+  for tag in ${IMAGE_TAG[@]}; do
+    if [[ $tag = "nonroot" ]]; then
+      bazel build base:base_nossl_nonroot_${DEBIAN_VERSION}
+    else
+      bazel build base:base_nossl_debug_nonroot_${DEBIAN_VERSION}
+    fi
+  done
+
+  # shellcheck disable=SC2068
+  for tag in ${IMAGE_TAG[@]}; do
+    # shellcheck disable=SC2068
+    # shellcheck disable=SC2034
+    manifest=${REPO}/base-nossl-${DEBIAN_VERSION}:${tag}
+    for arch in ${ARCH[@]}; do
+      # shellcheck disable=SC2128
+
+      image=${REPO}/base-${DEBIAN_VERSION}:${tag}-${arch}
+      if [[ $tag = "nonroot" ]]; then
+        oci_dir=oci:bazel-bin/base/base_nossl_nonroot_${arch}_${DEBIAN_VERSION}
+      else
+        oci_dir=oci:bazel-bin/base/base_nossl_debug_nonroot_${arch}_${DEBIAN_VERSION}
+      fi
+      skopeo copy $oci_dir docker-daemon:$image
+      docker manifest create $manifest -a $image
+    done
+    docker manifest push $manifest
+  done
+}
+
 function build-static() {
   # shellcheck disable=SC2068
   for tag in ${IMAGE_TAG[@]}; do
@@ -122,6 +153,8 @@ if [[ $CMD = "nodejs" ]]; then
   build-nodejs
 elif [[ $CMD = "base" ]]; then
   build-base
+elif [[ $CMD = "base-nossl" ]]; then
+  build-base-nossl
 elif [[ $CMD = "static" ]]; then
   build-static
 fi
