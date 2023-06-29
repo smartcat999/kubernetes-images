81becc4470606f52d6c641cddb5d582fb4cfffff
peng wu <2030047311@qq.com>
2023-06-29 11:53:07 +0800

    feat: update nodegroup apis mock.sh

--- tools/mock.sh
+++ tools/mock.sh
@@ -15,8 +15,8 @@ function error() {
 
 KS_APISERVER=""
 MEMBER_CLUSTER=""
-USER=""
-PASS=""
+USER=${USER:-}
+PASS=${PASS:-}
 if [ "$USER" = "" ]; then
   error "Please set USER=user PASS=*** !"
   exit 1
@@ -112,17 +112,251 @@ function delete-vnode() {
   kubectl get nodes -l vnode=mock | awk '{if (NR>1) print $1}' | xargs kubectl delete node
 }
 
+function create-role-template() {
+  # create GlobalRole
+  cat <<EOF | kubectl apply -f -
+apiVersion: iam.kubesphere.io/v1alpha2
+kind: GlobalRole
+metadata:
+  annotations:
+    iam.kubesphere.io/module: Access Control
+    iam.kubesphere.io/role-template-rules: '{"nodegroups": "view"}'
+    kubesphere.io/alias-name: NodeGroups View
+  labels:
+    iam.kubesphere.io/role-template: 'true'
+    kubefed.io/managed: 'true'
+  name: role-template-view-nodegroups
+rules:
+  - apiGroups:
+      - 'infra.kubesphere.io'
+    resources:
+      - 'nodegroups'
+      - 'nodes'
+      - 'namespaces'
+    verbs:
+      - 'list'
+      - 'get'
+      - 'watch'
+  - apiGroups:
+      - 'iam.kubesphere.io'
+    resources:
+      - 'roles'
+      - 'members'
+      - 'members/nodegrouproles'
+    verbs:
+      - 'list'
+      - 'get'
+      - 'watch'
+
+---
+apiVersion: iam.kubesphere.io/v1alpha2
+kind: GlobalRole
+metadata:
+  annotations:
+    iam.kubesphere.io/dependencies: '["role-template-view-nodegroups"]'
+    iam.kubesphere.io/module: Access Control
+    iam.kubesphere.io/role-template-rules: '{"nodegroups": "manage"}'
+    kubesphere.io/alias-name: NodeGroups Management
+  labels:
+    iam.kubesphere.io/role-template: 'true'
+    kubefed.io/managed: 'true'
+  name: role-template-manage-nodegroups
+rules:
+  - apiGroups:
+      - 'infra.kubesphere.io'
+    resources:
+      - 'nodegroups'
+      - 'nodes'
+      - 'namespaces'
+    verbs:
+      - '*'
+  - apiGroups:
+      - 'iam.kubesphere.io'
+    resources:
+      - 'roles'
+      - 'members'
+      - 'members/nodegrouproles'
+    verbs:
+      - '*'
+EOF
+  # create NodeGroupRole role-template
+  #  cat <<EOF | kubectl apply -f -
+  #apiVersion: iam.kubesphere.io/v1alpha2
+  #kind: NodeGroupRole
+  #metadata:
+  #  annotations:
+  #    iam.kubesphere.io/module: Access Control
+  #    iam.kubesphere.io/role-template-rules: '{"nodegroups": "view"}'
+  #    kubesphere.io/alias-name: NodeGroups View
+  #  labels:
+  #    iam.kubesphere.io/role-template: 'true'
+  #  name: role-template-view-nodegroups
+  #rules:
+  #  - apiGroups:
+  #      - 'infra.kubesphere.io'
+  #    resources:
+  #      - 'nodegroups'
+  #      - 'nodes'
+  #      - 'namespaces'
+  #    verbs:
+  #      - 'get'
+  #      - 'list'
+  #      - 'watch'
+  #
+  #---
+  #apiVersion: iam.kubesphere.io/v1alpha2
+  #kind: NodeGroupRole
+  #metadata:
+  #  annotations:
+  #    iam.kubesphere.io/dependencies: '["role-template-view-nodegroups"]'
+  #    iam.kubesphere.io/module: Access Control
+  #    iam.kubesphere.io/role-template-rules: '{"nodegroups": "manage"}'
+  #    kubesphere.io/alias-name: NodeGroups Management
+  #  labels:
+  #    iam.kubesphere.io/role-template: 'true'
+  #  name: role-template-manage-nodegroups
+  #rules:
+  #  - apiGroups:
+  #      - 'infra.kubesphere.io'
+  #    resources:
+  #      - 'nodegroups'
+  #    verbs:
+  #      - '*'
+  #  - apiGroups:
+  #      - 'iam.kubesphere.io'
+  #    resources:
+  #      - 'roles'
+  #      - 'members'
+  #      - 'members/nodegrouproles'
+  #    verbs:
+  #      - '*'
+  #EOF
+
+  # init RoleBase
+  cat <<EOF | kubectl apply -f -
+apiVersion: iam.kubesphere.io/v1alpha2
+kind: RoleBase
+metadata:
+  name: nodegroup-viewer
+role:
+  apiVersion: iam.kubesphere.io/v1alpha2
+  kind: NodeGroupRole
+  metadata:
+    name: viewer
+  rules:
+    - apiGroups:
+        - 'infra.kubesphere.io'
+      resources:
+        - 'nodegroups'
+        - 'nodes'
+        - 'namespaces'
+      verbs:
+        - 'get'
+        - 'list'
+        - 'watch'
+
+---
+apiVersion: iam.kubesphere.io/v1alpha2
+kind: RoleBase
+metadata:
+  name: nodegroup-admin
+role:
+  apiVersion: iam.kubesphere.io/v1alpha2
+  kind: NodeGroupRole
+  metadata:
+    name: admin
+  rules:
+    - apiGroups:
+        - 'infra.kubesphere.io'
+      resources:
+        - 'nodegroups'
+      verbs:
+        - '*'
+    - apiGroups:
+        - 'iam.kubesphere.io'
+      resources:
+        - 'roles'
+        - 'members'
+        - 'members/nodegrouproles'
+      verbs:
+        - '*'
+EOF
+}
+
+function delete-role-template() {
+  kubectl delete GlobalRole role-template-view-nodegroups role-template-manage-nodegroups
+  kubectl delete RoleBase nodegroup-viewer nodegroup-admin
+  #  kubectl delete NodeGroupRole role-template-view-nodegroups role-template-manage-nodegroups
+}
+
+# create NodeGroupRole
+function create-nodegroup-role() {
+  nodegroup=$1
+  cat <<EOF | kubectl apply -f -
+apiVersion: iam.kubesphere.io/v1alpha2
+kind: NodeGroupRole
+metadata:
+  annotations:
+    iam.kubesphere.io/aggregation-roles: >-
+      ["role-template-view-nodegroups"]
+    kubesphere.io/creator: system
+  labels:
+    kubesphere.io/nodegroup: $nodegroup
+  name: $nodegroup-viewer
+rules:
+  - apiGroups:
+      - '*'
+    resources:
+      - '*'
+    verbs:
+      - get
+      - list
+      - watch
+
+---
+apiVersion: iam.kubesphere.io/v1alpha2
+kind: NodeGroupRole
+metadata:
+  annotations:
+    iam.kubesphere.io/aggregation-roles: >-
+      ["role-template-manage-nodegroups", "role-template-view-nodegroups"]
+    kubesphere.io/creator: system
+  labels:
+    kubesphere.io/nodegroup: $nodegroup
+  name: $nodegroup-admin
+rules:
+  - apiGroups:
+      - '*'
+    resources:
+      - '*'
+    verbs:
+      - '*'
+EOF
+}
+
+function delete-nodegroup-role() {
+  nodegroup=$1
+  kubectl delete NodeGroupRole $nodegroup-viewer $nodegroup-admin
+}
+
 case "$1" in
 "create")
+  create-role-template
+
   for ((i = 1; i < 10; i++)); do
     create-nodegroup nodegroup0$i
     create-vnode vnode0$i
   done
   ;;
+"delete")
+  delete-nodegroup $2
+  ;;
 "clean")
   for ((i = 1; i < 10; i++)); do
     delete-nodegroup nodegroup0$i
   done
+
+  delete-role-template
   delete-vnode
   ;;
 esac
