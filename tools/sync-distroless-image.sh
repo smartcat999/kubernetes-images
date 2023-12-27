dee0f5e3d07f6c8dba31f2b5909ebcd6979721c2
peng wu <2030047311@qq.com>
2023-12-27 14:17:12 +0800

    feat: update build distroless

--- tools/sync-distroless-image.sh
+++ tools/sync-distroless-image.sh
@@ -7,7 +7,10 @@ fi
 CMD=$1
 
 REPO=${REPO:-2030047311}
-DEBIAN_VERSION=debian12
+DEBIAN_VERSIONS=(
+  debian11
+  debian12
+)
 ARCH=(
   amd64
   arm64
@@ -25,11 +28,13 @@ function build-nodejs() {
   for tag in ${IMAGE_TAG[@]}; do
     # shellcheck disable=SC2068
     for version in ${NODEJS_VERSIONS[@]}; do
-      if [[ $tag = "nonroot" ]]; then
-        bazel build //nodejs:${version}_nonroot_${DEBIAN_VERSION}
-      else
-        bazel build //nodejs:${version}_debug_nonroot_${DEBIAN_VERSION}
-      fi
+      for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+        if [[ $tag = "nonroot" ]]; then
+          bazel build //nodejs:${version}_nonroot_${DEBIAN_VERSION}
+        else
+          bazel build //nodejs:${version}_debug_nonroot_${DEBIAN_VERSION}
+        fi
+      done
     done
   done
 
@@ -38,15 +43,54 @@ function build-nodejs() {
     # shellcheck disable=SC2068
     # shellcheck disable=SC2034
     for version in ${NODEJS_VERSIONS[@]}; do
-      manifest=${REPO}/${version}-${DEBIAN_VERSION}:${tag}
+      for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+        manifest=${REPO}/${version}-${DEBIAN_VERSION}:${tag}
+        for arch in ${ARCH[@]}; do
+          # shellcheck disable=SC2128
+
+          image=${REPO}/${version}-${DEBIAN_VERSION}:${tag}-${arch}
+          if [[ $tag = "nonroot" ]]; then
+            oci_dir=oci:bazel-bin/nodejs/${version}_nonroot_${arch}_${DEBIAN_VERSION}
+          else
+            oci_dir=oci:bazel-bin/nodejs/${version}_debug_nonroot_${arch}_${DEBIAN_VERSION}
+          fi
+          skopeo copy $oci_dir docker-daemon:$image
+          docker push $image
+          docker manifest create $manifest -a $image
+        done
+        docker manifest push $manifest
+        docker manifest rm $manifest
+      done
+    done
+  done
+}
+
+function build-base() {
+  # shellcheck disable=SC2068
+  for tag in ${IMAGE_TAG[@]}; do
+    for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+      if [[ $tag = "nonroot" ]]; then
+        bazel build //base:base_nonroot_${DEBIAN_VERSION}
+      else
+        bazel build //base:debug_nonroot_${DEBIAN_VERSION}
+      fi
+    done
+  done
+
+  # shellcheck disable=SC2068
+  for tag in ${IMAGE_TAG[@]}; do
+    for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+      # shellcheck disable=SC2068
+      # shellcheck disable=SC2034
+      manifest=${REPO}/base-${DEBIAN_VERSION}:${tag}
       for arch in ${ARCH[@]}; do
         # shellcheck disable=SC2128
 
-        image=${REPO}/${version}-${DEBIAN_VERSION}:${tag}-${arch}
+        image=${REPO}/base-${DEBIAN_VERSION}:${tag}-${arch}
         if [[ $tag = "nonroot" ]]; then
-          oci_dir=oci:bazel-bin/nodejs/${version}_nonroot_${arch}_${DEBIAN_VERSION}
+          oci_dir=oci:bazel-bin/base/base_nonroot_${arch}_${DEBIAN_VERSION}
         else
-          oci_dir=oci:bazel-bin/nodejs/${version}_debug_nonroot_${arch}_${DEBIAN_VERSION}
+          oci_dir=oci:bazel-bin/base/debug_nonroot_${arch}_${DEBIAN_VERSION}
         fi
         skopeo copy $oci_dir docker-daemon:$image
         docker push $image
@@ -58,107 +102,82 @@ function build-nodejs() {
   done
 }
 
-function build-base() {
-  # shellcheck disable=SC2068
-  for tag in ${IMAGE_TAG[@]}; do
-    if [[ $tag = "nonroot" ]]; then
-      bazel build //base:base_nonroot_${DEBIAN_VERSION}
-    else
-      bazel build //base:debug_nonroot_${DEBIAN_VERSION}
-    fi
-  done
-
-  # shellcheck disable=SC2068
-  for tag in ${IMAGE_TAG[@]}; do
-    # shellcheck disable=SC2068
-    # shellcheck disable=SC2034
-    manifest=${REPO}/base-${DEBIAN_VERSION}:${tag}
-    for arch in ${ARCH[@]}; do
-      # shellcheck disable=SC2128
-
-      image=${REPO}/base-${DEBIAN_VERSION}:${tag}-${arch}
-      if [[ $tag = "nonroot" ]]; then
-        oci_dir=oci:bazel-bin/base/base_nonroot_${arch}_${DEBIAN_VERSION}
-      else
-        oci_dir=oci:bazel-bin/base/debug_nonroot_${arch}_${DEBIAN_VERSION}
-      fi
-      skopeo copy $oci_dir docker-daemon:$image
-      docker push $image
-      docker manifest create $manifest -a $image
-    done
-    docker manifest push $manifest
-    docker manifest rm $manifest
-  done
-}
-
 # bazel query //base:*
 function build-base-nossl() {
   # shellcheck disable=SC2068
   for tag in ${IMAGE_TAG[@]}; do
-    if [[ $tag = "nonroot" ]]; then
-      # //base:base_nossl_nonroot_debian12
-      bazel build //base:base_nossl_nonroot_${DEBIAN_VERSION}
-    else
-      bazel build //base:base_nossl_debug_nonroot_${DEBIAN_VERSION}
-    fi
+    for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+      if [[ $tag = "nonroot" ]]; then
+        # //base:base_nossl_nonroot_debian12
+        bazel build //base:base_nossl_nonroot_${DEBIAN_VERSION}
+      else
+        bazel build //base:base_nossl_debug_nonroot_${DEBIAN_VERSION}
+      fi
+    done
   done
 
   # shellcheck disable=SC2068
   for tag in ${IMAGE_TAG[@]}; do
     # shellcheck disable=SC2068
     # shellcheck disable=SC2034
-    manifest=${REPO}/base-nossl-${DEBIAN_VERSION}:${tag}
-    for arch in ${ARCH[@]}; do
-      # shellcheck disable=SC2128
+    for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+      manifest=${REPO}/base-nossl-${DEBIAN_VERSION}:${tag}
+      for arch in ${ARCH[@]}; do
+        # shellcheck disable=SC2128
 
-      image=${REPO}/base-nossl-${DEBIAN_VERSION}:${tag}-${arch}
-      echo "$manifest"
-      echo "$image"
+        image=${REPO}/base-nossl-${DEBIAN_VERSION}:${tag}-${arch}
+        echo "$manifest"
+        echo "$image"
 
-      if [[ $tag = "nonroot" ]]; then
-        oci_dir=oci:bazel-bin/base/base_nossl_nonroot_${arch}_${DEBIAN_VERSION}
-      else
-        oci_dir=oci:bazel-bin/base/base_nossl_debug_nonroot_${arch}_${DEBIAN_VERSION}
-      fi
-      skopeo copy $oci_dir docker-daemon:$image
-      docker push $image
-      docker manifest create $manifest -a $image
+        if [[ $tag = "nonroot" ]]; then
+          oci_dir=oci:bazel-bin/base/base_nossl_nonroot_${arch}_${DEBIAN_VERSION}
+        else
+          oci_dir=oci:bazel-bin/base/base_nossl_debug_nonroot_${arch}_${DEBIAN_VERSION}
+        fi
+        skopeo copy $oci_dir docker-daemon:$image
+        docker push $image
+        docker manifest create $manifest -a $image
+      done
+      docker manifest push $manifest
+      docker manifest rm $manifest
     done
-    docker manifest push $manifest
-    docker manifest rm $manifest
   done
 }
 
 function build-static() {
   # shellcheck disable=SC2068
   for tag in ${IMAGE_TAG[@]}; do
-    if [[ $tag = "nonroot" ]]; then
-      bazel build //base:static_nonroot_${DEBIAN_VERSION}
-    else
-      bazel build //base:static_debug_nonroot_${DEBIAN_VERSION}
-    fi
+    for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+      if [[ $tag = "nonroot" ]]; then
+        bazel build //base:static_nonroot_${DEBIAN_VERSION}
+      else
+        bazel build //base:static_debug_nonroot_${DEBIAN_VERSION}
+      fi
+    done
   done
 
   # shellcheck disable=SC2068
   for tag in ${IMAGE_TAG[@]}; do
     # shellcheck disable=SC2068
     # shellcheck disable=SC2034
-    manifest=${REPO}/static-${DEBIAN_VERSION}:${tag}
-    for arch in ${ARCH[@]}; do
-      # shellcheck disable=SC2128
+    for DEBIAN_VERSION in ${DEBIAN_VERSIONS[@]}; do
+      manifest=${REPO}/static-${DEBIAN_VERSION}:${tag}
+      for arch in ${ARCH[@]}; do
+        # shellcheck disable=SC2128
 
-      image=${REPO}/static-${DEBIAN_VERSION}:${tag}-${arch}
-      if [[ $tag = "nonroot" ]]; then
-        oci_dir=oci:bazel-bin/base/static_nonroot_${arch}_${DEBIAN_VERSION}
-      else
-        oci_dir=oci:bazel-bin/base/static_debug_nonroot_${arch}_${DEBIAN_VERSION}
-      fi
-      skopeo copy $oci_dir docker-daemon:$image
-      docker push $image
-      docker manifest create $manifest -a $image
+        image=${REPO}/static-${DEBIAN_VERSION}:${tag}-${arch}
+        if [[ $tag = "nonroot" ]]; then
+          oci_dir=oci:bazel-bin/base/static_nonroot_${arch}_${DEBIAN_VERSION}
+        else
+          oci_dir=oci:bazel-bin/base/static_debug_nonroot_${arch}_${DEBIAN_VERSION}
+        fi
+        skopeo copy $oci_dir docker-daemon:$image
+        docker push $image
+        docker manifest create $manifest -a $image
+      done
+      docker manifest push $manifest
+      docker manifest rm $manifest
     done
-    docker manifest push $manifest
-    docker manifest rm $manifest
   done
 }
 
